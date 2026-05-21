a7c01ce44c8bfbae05b7b5545daef8d8de3fd380
sasmith-14 <suvarnasasmith14@gmail.com>
2026-05-21 22:12:46 +0530

    db testing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const multer = require('multer');
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/post', upload.single('image'), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    res.json({ message: "Post created successfully" });
+})
 
 module.exports = app;
